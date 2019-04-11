c11927feff37138d76515d66471e166342ca08fc
jarjune <408100801@qq.com>
2019-04-11 15:57:29 +0800

    rename

--- spring-cloud-eureka-client-feign-consumer-demo/src/main/java/com/jarjune/demo/feign/controller/FeignConsumerController.java
+++ spring-cloud-eureka-client-feign-consumer-demo/src/main/java/com/jarjune/demo/feign/controller/FeignConsumerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FeignConsimerController {
+public class FeignConsumerController {
 
     @Autowired
     private EurekaClientFeignService eurekaClientFeignService;
